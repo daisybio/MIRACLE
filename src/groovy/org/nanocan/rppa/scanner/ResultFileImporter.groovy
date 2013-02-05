157148f6ecad175f91c6a13114ac6e3d4fe24567
mlist <mlist@health.sdu.dk>
2013-02-05 06:10:11 +0100

    partial fix for excel memory problem + fix for delete spots bugg

--- src/groovy/org/nanocan/rppa/scanner/ResultFileImporter.groovy
+++ src/groovy/org/nanocan/rppa/scanner/ResultFileImporter.groovy
@@ -9,10 +9,71 @@ package org.nanocan.rppa.scanner
 import org.grails.plugins.excelimport.*
 import static org.grails.plugins.excelimport.ExpectedPropertyType.*
 import org.nanocan.rppa.scanner.ResultFileConfig
+import org.apache.commons.io.FilenameUtils
+import org.apache.poi.openxml4j.opc.OPCPackage
+import org.apache.poi.xssf.eventusermodel.XSSFReader
+import org.apache.poi.ss.usermodel.WorkbookFactory
+import org.apache.poi.openxml4j.opc.PackageAccess
+import org.apache.poi.xssf.streaming.SXSSFWorkbook
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
 class ResultFileImporter extends AbstractExcelImporter{
 
     def static cellReporter = new DefaultImportCellCollector()
+
+    static getSheets(filePath){
+
+        def fileEnding = FilenameUtils.getExtension(filePath)
+        def sheets = []
+
+
+        if(fileEnding == "xlsx")
+        {
+            OPCPackage pkg = OPCPackage.open(filePath, PackageAccess.READ);
+
+            XSSFReader r = new XSSFReader(pkg)
+            XSSFReader.SheetIterator iterator = (XSSFReader.SheetIterator) r.getSheetsData()
+
+            while(iterator.hasNext())
+            {
+                iterator.next()
+                sheets << iterator.getSheetName()
+            }
+        }
+
+        else {
+            def workbook = WorkbookFactory.create(new File(filePath));
+            def numOfSheets = workbook.getNumberOfSheets()
+            for (int i = 0; i < numOfSheets; i++)
+            {
+                sheets << workbook.getSheetName(i)
+            }
+        }
+
+        return sheets
+    }
+
+    protected def customRead(filePath)
+    {
+        def fileEnding = FilenameUtils.getExtension(filePath)
+
+        if(fileEnding == "xlsx") readXLSX(filePath)
+        else readXLS(filePath)
+    }
+
+    protected def readXLSX(filePath)
+    {
+        OPCPackage pkg = OPCPackage.open(filePath, PackageAccess.READ);
+        workbook = new SXSSFWorkbook(new XSSFWorkbook())
+        workbook = WorkbookFactory.create(pkg)
+        evaluator = workbook.creationHelper.createFormulaEvaluator()
+    }
+
+    protected def readXLS(filePath) {
+
+        workbook = WorkbookFactory.create(new File(filePath))
+        evaluator = workbook.creationHelper.createFormulaEvaluator()
+    }
 
     static Map configuratiomMap = [
             BG: ([expectedType: DoubleType, defaultValue: 0.0]),
@@ -29,18 +90,6 @@ class ResultFileImporter extends AbstractExcelImporter{
     public ResultFileImporter()
     {
         super()
-    }
-
-    Map getSheets()
-    {
-        def sheetNames = [:]
-
-        for(int i = 0;  i < workbook.getNumberOfSheets(); i++)
-        {
-            sheetNames[i] = workbook.getSheetName(i)
-        }
-
-        return(sheetNames)
     }
 
     List<Map> getSpots(String sheetName, ResultFileConfig rfc) {
