dbc7cc0353c709c24e1c0c191496dfcda3ea17a3
Markus List <mlist@health.sdu.dk>
2014-04-06 21:50:43 +0200

    added GPLv3 license to all files and license to root
    Former-commit-id: 1d5becc5037248f917f61df7edd575ca76108f08

--- src/templates/scaffolding/Controller.groovy
+++ src/templates/scaffolding/Controller.groovy
@@ -1,3 +1,32 @@
+/*
+ * Copyright (C) 2014
+ * Center for Excellence in Nanomedicine (NanoCAN)
+ * Molecular Oncology
+ * University of Southern Denmark
+ * ###############################################
+ * Written by:	Markus List
+ * Contact: 	mlist'at'health'.'sdu'.'dk
+ * Web:			http://www.nanocan.org/miracle/
+ * ###########################################################################
+ *	
+ *	This file is part of MIRACLE.
+ *
+ *  MIRACLE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with this program. It can be found at the root of the project page.
+ *	If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ############################################################################
+ */
 <%=packageName ? "package ${packageName}\n\n" : ''%>import org.springframework.dao.DataIntegrityViolationException
 
 class ${className}Controller {
